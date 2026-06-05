b5c3407f3646eeab493704767a00938f4dd8bd9f
om <om@DESKTOP-BSE9SNP>
2026-06-05 13:11:43 +0530

    Tl has developed some method

--- GitApp1/src/main/java/com/eshwar/main/CardPayment.java
+++ GitApp1/src/main/java/com/eshwar/main/CardPayment.java
@@ -1,5 +1,8 @@
 package com.eshwar.main;
-
-public class CardPayment {
-
+public class CardPayment
+{
+  public String doPaymentUsingCreditCard(Long cardNo,Double amount)
+  {
+	  return amount+" Ruppes Payment Done Sucessfully From the Card No : "+cardNo+" Using CreditCard";
+  }
 }
