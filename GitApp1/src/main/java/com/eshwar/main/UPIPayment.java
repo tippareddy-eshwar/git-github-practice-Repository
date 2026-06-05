889dd6c90c712fc02762965739d10b65f99bc46a
om <om@DESKTOP-BSE9SNP>
2026-06-05 12:40:12 +0530

    dev2 developed makingPaymentUsingGooglePay() method

--- GitApp1/src/main/java/com/eshwar/main/UPIPayment.java
+++ GitApp1/src/main/java/com/eshwar/main/UPIPayment.java
@@ -7,4 +7,11 @@ public class UPIPayment
 	   System.out.println("UPIPayment Class  makingPaymentUsingPhonePay(---,---) method is executed...");
 	   return amount+" Rupees Payment is Done Sucessfully From MobileNo "+mobileNo+" Using PhonePay";
    }
+   
+   public String makingPaymentUsingGooglePay(Long mobileNo,Double amount)
+   {
+	   System.out.println("UPIPayment Class  makingGooglePayUsingPhonePay(---,---) method is executed...");
+	   return amount+" Rupees Payment is Done Sucessfully From MobileNo "+mobileNo+" Using GooglePay";
+   }
+   
 }
