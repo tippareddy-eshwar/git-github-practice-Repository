facffcb06e1c273d046fdc5c1f42edb90402ac33
om <om@DESKTOP-BSE9SNP>
2026-06-05 12:23:54 +0530

    Dev1 devloped makingPaymentUsingPhonePay() method

---
+++ GitApp1/src/main/java/com/eshwar/main/UPIPayment.java
@@ -0,0 +1,10 @@
+//UPIPayment.java
+package com.eshwar.main;
+public class UPIPayment
+{
+   public String makingPaymentUsingPhonePay(Long mobileNo,Double amount)
+   {
+	   System.out.println("UPIPayment Class  makingPaymentUsingPhonePay(---,---) method is executed...");
+	   return amount+" Rupees Payment is Done Sucessfully From MobileNo "+mobileNo+" Using PhonePay";
+   }
+}
