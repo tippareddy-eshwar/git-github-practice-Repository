39c35435b582e214328d95b9d1a114c22ccfd0d1
om <om@DESKTOP-BSE9SNP>
2026-06-09 12:36:56 +0530

    TL has developed makingPaymentUsingPhonePayLite method

---
+++ GitApp1/src/main/java/com/eshwar/upilite/UPILiteTest.java
@@ -0,0 +1,11 @@
+package com.eshwar.upilite;
+
+public class UPILiteTest 
+{
+	public String makingPaymentUsingPhonePayLite(Long mobileNo,Double amount)
+	   {
+		   System.out.println("UPILiteTest Class  makingPaymentUsingPhonePayLite(---,---) method is executed...");
+		   return amount+" Rupees Payment is Done Sucessfully From MobileNo "+mobileNo+" Using PhonePayLite";
+	   }
+	   
+}
