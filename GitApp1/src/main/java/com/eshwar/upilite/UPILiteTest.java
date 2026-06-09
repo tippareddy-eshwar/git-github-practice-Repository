560ab6d3cdb5565b2d078b06555cd84eb4e371af
om <om@DESKTOP-BSE9SNP>
2026-06-09 12:42:44 +0530

    Dev1 developed makingPaymentUsingGooglePayLite method

--- GitApp1/src/main/java/com/eshwar/upilite/UPILiteTest.java
+++ GitApp1/src/main/java/com/eshwar/upilite/UPILiteTest.java
@@ -7,5 +7,11 @@ public class UPILiteTest
 		   System.out.println("UPILiteTest Class  makingPaymentUsingPhonePayLite(---,---) method is executed...");
 		   return amount+" Rupees Payment is Done Sucessfully From MobileNo "+mobileNo+" Using PhonePayLite";
 	   }
+	
+	public String makingPaymentUsingGooglePayLite(Long mobileNo,Double amount)
+	   {
+		   System.out.println("UPILiteTest Class  makingPaymentUsingGooglePayLite(---,---) method is executed...");
+		   return amount+" Rupees Payment is Done Sucessfully From MobileNo "+mobileNo+" Using GooglePayLite";
+	   }
 	   
 }
